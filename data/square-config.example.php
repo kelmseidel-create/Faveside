55307e8fbe7f8e87a9dd9a82171b0410371eb3a0
kelmseidel-create <kelmseidel@gmail.com>
2026-08-25 22:12:25 -0500

    Add private Square config template

---
+++ data/square-config.example.php
@@ -0,0 +1,14 @@
+<?php
+// Copy this file on the server to data/square-config.php and fill in production values.
+// Never commit the real square-config.php file.
+return [
+    'access_token' => '',
+    'location_id' => '',
+    'monthly_plan_variation_id' => '',
+    'annual_plan_variation_id' => '',
+    'monthly_checkout_url' => '',
+    'annual_checkout_url' => '',
+    'webhook_signature_key' => '',
+    'webhook_url' => 'https://faveside.com/api/square-webhook.php',
+    'api_base' => 'https://connect.squareup.com',
+];
